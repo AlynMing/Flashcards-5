798d8889b8cc67a883734ab60abd0329f182eaee
wndzhu3 <wndzhu3@gmail.com>
2020-02-29 13:56:05 -0500

    lab 2 updates

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var frontlabel: UILabel!
     @IBOutlet weak var backlabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -20,6 +21,23 @@ class ViewController: UIViewController {
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         self.frontlabel.isHidden = true
+    }
+    
+    func updateFlashcard(question: String, answer: String) {
+        frontlabel.text = question
+        backlabel.text = answer
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // we know the destination of the segue is the Navigation Controller
+        let navigationController = segue.destination as! UINavigationController
+        
+        // we know the Navigation Controller only contains a Creation View Controller
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        // we set the flashcardsController property to self
+        creationController.flashcardsController = self
     }
 }
 
